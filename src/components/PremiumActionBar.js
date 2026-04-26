a65b85de726138a8c572af93f810cc6f3e27f23a
Snehal05-sys <devkarsnehal0@gmail.com>
2026-04-26 07:23:03 +0530

    Update PremiumActionBar.js

--- src/components/PremiumActionBar.js
+++ src/components/PremiumActionBar.js
@@ -1,4 +1,4 @@
-import { speakText, pauseSpeech, resumeSpeech, stopSpeech } from "../utils/speechUtils";
+import { speakText, pauseSpeech, stopSpeech } from "../utils/speechUtils";
 
 export default function PremiumActionBar({ text = "", onToggleHindi, hindi = false, onSave, shareTitle = "Perth Numerology Reading" }) {
   function handleShare() {
@@ -14,7 +14,6 @@ export default function PremiumActionBar({ text = "", onToggleHindi, hindi = fal
     <div className="tool-row premium-toolbar">
       <button className="mini-btn" onClick={() => speakText(text)}>🔊 Play</button>
       <button className="mini-btn" onClick={pauseSpeech}>⏸ Pause</button>
-      <button className="mini-btn" onClick={resumeSpeech}>▶ Resume</button>
       <button className="mini-btn" onClick={stopSpeech}>⏹ Stop</button>
       {onToggleHindi && (
         <button className="mini-btn" onClick={onToggleHindi}>{hindi ? "English" : "Translate to Hindi"}</button>
